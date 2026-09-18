25b00bf3316653fc90d4e12ae399cf233ce42351
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-09-19 04:42:24 +0800

    Initial Android app with APK build workflow

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "tw.pulipuli.share2draft"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "tw.pulipuli.share2draft"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
+dependencies {}
